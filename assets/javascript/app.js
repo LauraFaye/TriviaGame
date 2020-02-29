86e9123a385a47c22138c1b9e7959b7e82e33b35
Laura Faye <jim.laura2@tds.net>
2020-02-29 09:02:11 -0500

    added js and modified html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,30 @@
 $(document).ready(function () {
+
+var answers={
+    questionOne:true,
+    questionTwo:false
+}
+var correct=0
+var incorrect=0
+
+
     $("#Submit").click(function () {
-        console.log("I am working")
+    
+        var radioValue1 = $("input[name='question1']:checked").val();
+        var radioValue2 = $("input[name='question2']:checked").val();
+if (radioValue1 === "true"){
+correct+=1
+}
+if (radioValue2 === "false"){
+    correct+=1
+}
+
+
+        console.log(radioValue)
     })
+
+
+
 })
 
+// var radioValue = $("input[name='gender']:checked").val();
